44a45318c948e4e99c4aa12fc4f9deec00166353
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-06-10 19:39:53 +0530

    image picker added

--- ImageToPdfConverter/ViewController.swift
+++ ImageToPdfConverter/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import ImagePicker
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,ImagePickerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,17 +25,34 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "Image import option", message: "select the type to option fit for importing image", preferredStyle: .actionSheet)
         
         let  photoLibrary = UIAlertAction(title: "Photo library", style: .default) { (UIAlertAction) in
-            print("yes")
+            let imagePickerController = ImagePickerController()
+            imagePickerController.delegate = self
+            self.present(imagePickerController, animated: true, completion: nil)
         }
         
         let  camera = UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in
-            print("NO")
+            print("NO PHONE")
         }
         alert.addAction(photoLibrary)
         alert.addAction(camera)
         present(alert, animated: true, completion: nil)
         
     }
+    
+    
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        print(images.count)
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
 
 
 }
